0047e786fc24b8bc61d61b70b6a5375f3a46df1a
aZong <giongmua2004@gmail.com>
2025-08-29 15:26:31 +0700

    feat(module): user module

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { join } from 'path';
       rootPath: join(process.cwd(), 'public'),
     }),
     ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
