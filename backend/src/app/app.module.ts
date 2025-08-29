4c8a69c09b3d3f51c78ee4c3c5e8bb9bb77a972b
aZong <giongmua2004@gmail.com>
2025-08-29 11:04:35 +0700

    feat(module): ServeStaticModule

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public'),
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
