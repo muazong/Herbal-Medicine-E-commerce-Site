af167cd05e7803bf93dcbdeeb8de7268b99292fa
aZong <giongmua2004@gmail.com>
2025-08-29 12:58:50 +0700

    feat(properties): createdAt & updatedAt properties for abstract class

--- backend/src/database/abstract.entity.ts
+++ backend/src/database/abstract.entity.ts
@@ -1,8 +1,18 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class AbstractEntity<T> {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 
   constructor(entity: Partial<T>) {
     Object.assign(this, entity);
