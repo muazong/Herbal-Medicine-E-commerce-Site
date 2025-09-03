f778924c3569af4a8a478ecf2adba8016ece7072
aZong <giongmua2004@gmail.com>
2025-09-03 11:00:19 +0700

    feat(dto): User dtos

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,42 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { User } from '../entities/user.entity';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Length,
+  MinLength,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(
+  OmitType(User, ['id', 'createdAt', 'role', 'status'] as const),
+) {
+  @IsOptional()
+  @IsString()
+  @Length(2, 50)
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  lastName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 255)
+  username?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(0, 100)
+  address?: string;
+}
