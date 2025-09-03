b8b59d148541570ce284b9228a76858b1baf903a
aZong <giongmua2004@gmail.com>
2025-09-03 08:57:19 +0700

    feat(entity): User entity

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,6 +1,46 @@
-import { IsString } from 'class-validator';
-import { AbstractEntity } from 'src/database/abstract.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
+import { AccountStatus, Role } from '../../common/enums';
+import { AbstractEntity } from '../../database/abstract.entity';
+import { DefaultImages } from '../../common/contances';
 
-@Entity()
-export class User extends AbstractEntity<User> {}
+@Entity({ name: 'users' })
+export class User extends AbstractEntity<User> {
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  address?: string;
+
+  @Column({ default: DefaultImages.AVATAR })
+  avatar?: string;
+
+  @Column({ default: DefaultImages.COVER })
+  cover?: string;
+
+  @Column()
+  fullName: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.CLIENT })
+  role: Role;
+
+  @Column({ type: 'enum', enum: AccountStatus, default: AccountStatus.ACTIVE })
+  status: AccountStatus;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  generateFullNameOnUpdate(): void {
+    this.fullName = `${this.firstName} ${this.lastName}`;
+  }
+}
