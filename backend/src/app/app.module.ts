25b9dadf7133e0b44d0cf561ce09e6b513fb51c2
aZong <giongmua2004@gmail.com>
2025-09-05 23:24:33 +0700

    feat: add more root path

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,15 +1,27 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { DatabaseModule } from '../database/database.module';
+
 import { UsersModule } from '../users/users.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'public'),
-    }),
+    ServeStaticModule.forRoot(
+      {
+        rootPath: join(process.cwd(), 'public'),
+        serveRoot: '/',
+      },
+      {
+        rootPath: join(process.cwd(), 'uploads/users'),
+        serveRoot: '/users',
+      },
+      {
+        rootPath: join(process.cwd(), 'uploads/products'),
+        serveRoot: '/products',
+      },
+    ),
     ConfigModule.forRoot({ isGlobal: true }),
 
     DatabaseModule,
