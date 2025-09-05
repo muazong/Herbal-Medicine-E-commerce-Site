5daa0cc14e9ebc18e93cfd12dd52e61223115002
aZong <giongmua2004@gmail.com>
2025-09-05 23:32:19 +0700

    feat: export user service and import media module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,12 +1,19 @@
-import { Logger, Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { forwardRef, Module } from '@nestjs/common';
+
 import { User } from './entities/user.entity';
+import { Media } from '../media/entities/media.entity';
+import { UsersService } from './users.service';
+import { MediaModule } from '../media/media.module';
+import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User, Media]),
+    forwardRef(() => MediaModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
