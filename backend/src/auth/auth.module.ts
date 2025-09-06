af8b92acd555b3fa648b2aeed26e7e6c9b9d8ca5
aZong <giongmua2004@gmail.com>
2025-09-06 17:24:34 +0700

    feat: User repository for auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
