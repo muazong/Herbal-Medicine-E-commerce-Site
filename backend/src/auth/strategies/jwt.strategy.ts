84540d3ef1861574218d6178b5633f4708e189e0
aZong <giongmua2004@gmail.com>
2025-09-08 22:28:17 +0700

    feat(strategy): jwt strategy

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.sub };
+  }
+}
