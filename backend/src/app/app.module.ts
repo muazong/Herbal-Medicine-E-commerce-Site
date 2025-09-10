434f3d566d993adf008737a0d46d9c716034af8c
aZong <giongmua2004@gmail.com>
2025-09-10 23:30:18 +0700

    fix: modules path

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
-import { AuthModule } from '../auth/auth.module';
-import { UsersModule } from '../users/users.module';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from '../modules/database/database.module';
+import { UsersModule } from '../modules/users/users.module';
+import { AuthModule } from '../modules/auth/auth.module';
 
 @Module({
   imports: [
