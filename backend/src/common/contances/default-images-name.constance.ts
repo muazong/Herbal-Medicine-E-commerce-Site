f4cd8695350a4daa82257fdaceb8dc33cc3f66a1
aZong <giongmua2004@gmail.com>
2025-09-10 23:30:31 +0700

    feat: default images name constances

---
+++ backend/src/common/contances/default-images-name.constance.ts
@@ -0,0 +1,6 @@
+export const DefaultImagesName = {
+  LOCAL_AVATAR: 'Default Avatar',
+  LOCAL_COVER: 'Default Cover',
+  GOOGLE_AVATAR: "User's Google Avatar",
+  GITHUB_AVATAR: "User's Github Avatar",
+};
