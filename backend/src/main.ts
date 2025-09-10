2fb7b2d0e187889ba9d0fe64947c2c21ed67267e
aZong <giongmua2004@gmail.com>
2025-09-10 23:50:33 +0700

    fix: update path

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
-
-import { AppModule } from './app/app.module';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
