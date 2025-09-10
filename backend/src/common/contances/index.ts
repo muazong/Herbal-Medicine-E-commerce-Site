5af664e656764c605a3aba8a310fa58ea4b67382
aZong <giongmua2004@gmail.com>
2025-09-10 23:30:38 +0700

    feat: constances

--- backend/src/common/contances/index.ts
+++ backend/src/common/contances/index.ts
@@ -1,3 +1,5 @@
-import { DefaultImages } from './default-images.constance';
+import { DefaultImagesPath } from './default-images.constance';
+import { InitialUserMedia } from './initial-user-media.constance';
+import { DefaultImagesName } from './default-images-name.constance';
 
-export { DefaultImages };
+export { DefaultImagesPath, DefaultImagesName, InitialUserMedia };
