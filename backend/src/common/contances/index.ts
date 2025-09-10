2ce8fa59ffedd2eeff1ec7412584d8aeb3539581
aZong <giongmua2004@gmail.com>
2025-09-11 00:01:08 +0700

    feat(constance): roles key

--- backend/src/common/contances/index.ts
+++ backend/src/common/contances/index.ts
@@ -1,5 +1,6 @@
 import { DefaultImagesPath } from './default-images.constance';
 import { InitialUserMedia } from './initial-user-media.constance';
 import { DefaultImagesName } from './default-images-name.constance';
+import { ROLES_KEY } from './roles-key.contance';
 
-export { DefaultImagesPath, DefaultImagesName, InitialUserMedia };
+export { DefaultImagesPath, DefaultImagesName, InitialUserMedia, ROLES_KEY };
