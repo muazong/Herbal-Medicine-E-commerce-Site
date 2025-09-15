c563849d07ad5c02026ff80f00304455f08d02ab
aZong <giongmua2004@gmail.com>
2025-09-15 23:06:07 +0700

    feat: set role based and update findAll method

--- backend/src/modules/products/products.controller.ts
+++ backend/src/modules/products/products.controller.ts
@@ -1,4 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Get,
+  Post,
+  Body,
+  Query,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  UseGuards,
+  HttpStatus,
+  Controller,
+} from '@nestjs/common';
+
+import { Role } from '../../common/enums';
+import { JwtAuthGuard } from '../auth/guards';
+import { Roles } from '../../common/decorators';
+import { RolesGuard } from '../../common/guards';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -8,27 +25,39 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  findAll(
+    @Query('limit') limit: number,
+    @Query('page') page: number,
+    @Query('sort') sort: 'asc' | 'desc',
+    @Query('search') search: string,
+  ) {
+    return this.productsService.findAll(limit, page, sort, search);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+    return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+    return this.productsService.remove(id);
   }
 }
