aa270b142db6f9253ca23f758b3e949a797985ae
aZong <giongmua2004@gmail.com>
2025-09-15 23:05:34 +0700

    feat: product entity

--- backend/src/modules/products/entities/product.entity.ts
+++ backend/src/modules/products/entities/product.entity.ts
@@ -1,1 +1,28 @@
-export class Product {}
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+import { AbstractEntity } from '../../database/abstract.entity';
+import { Category } from '../../categories/entities/category.entity';
+
+@Entity({ name: 'products' })
+export class Product extends AbstractEntity<Product> {
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ nullable: true })
+  description?: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+
+  @Column()
+  stock: number;
+
+  @Column({ type: 'decimal', precision: 2, scale: 1 })
+  rating: number;
+
+  @ManyToOne(() => Category, (category) => category.products, {
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn()
+  category: Category;
+}
