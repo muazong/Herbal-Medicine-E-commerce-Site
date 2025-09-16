8b2afc88355b78fe4946d5902745cd61b972bb73
aZong <giongmua2004@gmail.com>
2025-09-16 21:37:13 +0700

    feat: http code

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -33,6 +33,7 @@ export class UsersController {
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll(
     @Query('limit') limit: number,
     @Query('page') page: number,
@@ -42,6 +43,7 @@ export class UsersController {
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
@@ -49,6 +51,7 @@ export class UsersController {
   @Patch(':id')
   @Roles(Role.ADMIN, Role.CLIENT)
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
@@ -56,6 +59,7 @@ export class UsersController {
   @Delete(':id')
   @Roles(Role.ADMIN, Role.CLIENT)
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
