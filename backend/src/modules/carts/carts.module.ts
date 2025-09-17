c864dd6d33ba78b9d7a0155a4e8c79b01ea162ce
aZong <giongmua2004@gmail.com>
2025-09-17 09:44:27 +0700

    feat: carts module

--- backend/src/modules/carts/carts.module.ts
+++ backend/src/modules/carts/carts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CartsController } from './carts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from '../cart-items/entities/cart-item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart, CartItem])],
   controllers: [CartsController],
   providers: [CartsService],
 })
--- backend/src/modules/carts/entities/cart.entity.ts
+++ backend/src/modules/carts/entities/cart.entity.ts
@@ -1,1 +1,15 @@
-export class Cart {}
+import { Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
+
+import { User } from '../../users/entities/user.entity';
+import { AbstractEntity } from '../../database/abstract.entity';
+import { CartItem } from '../../cart-items/entities/cart-item.entity';
+
+@Entity({ name: 'carts' })
+export class Cart extends AbstractEntity<Cart> {
+  @OneToOne(() => User, (user) => user.cart, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  user: User;
+
+  @OneToMany(() => CartItem, (cartItem) => cartItem.cart, { nullable: true })
+  cartItems?: CartItem[];
+}
