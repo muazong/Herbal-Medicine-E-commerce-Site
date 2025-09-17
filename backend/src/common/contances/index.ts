177540b82d9779302eacd2fecd1ecfce58c4a9a7
aZong <giongmua2004@gmail.com>
2025-09-17 09:38:59 +0700

    feat(constance): public key

--- backend/src/common/contances/index.ts
+++ backend/src/common/contances/index.ts
@@ -2,5 +2,12 @@ import { DefaultImagesPath } from './default-images.constance';
 import { InitialUserMedia } from './initial-user-media.constance';
 import { DefaultImagesName } from './default-images-name.constance';
 import { ROLES_KEY } from './roles-key.contance';
+import { IS_PUBLIC_KEY } from './public-key.constance';
 
-export { DefaultImagesPath, DefaultImagesName, InitialUserMedia, ROLES_KEY };
+export {
+  DefaultImagesPath,
+  DefaultImagesName,
+  InitialUserMedia,
+  ROLES_KEY,
+  IS_PUBLIC_KEY,
+};
