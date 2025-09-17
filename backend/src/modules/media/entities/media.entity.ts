398c72884c5127c3e566da8c68ae7f9cacd75989
aZong <giongmua2004@gmail.com>
2025-09-17 09:44:09 +0700

    docs: comment

--- backend/src/modules/media/entities/media.entity.ts
+++ backend/src/modules/media/entities/media.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 import { AbstractEntity } from '../../database/abstract.entity';
 import { MediaType } from '../../../common/enums';
+import { Category } from '../../categories/entities/category.entity';
 
 @Entity({ name: 'media' })
 export class Media extends AbstractEntity<Media> {
@@ -18,4 +19,11 @@ export class Media extends AbstractEntity<Media> {
 
   @Column({ type: 'enum', enum: MediaType })
   type: MediaType;
+
+  // TODO: Add relationship
+  /* @ManyToOne(() => Category, (category) => category.images, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  category: Category; */
 }
