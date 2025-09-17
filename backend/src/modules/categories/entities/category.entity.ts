bf4be4d1c98297dd4311bdb82580cd0897ca7a85
aZong <giongmua2004@gmail.com>
2025-09-17 09:41:23 +0700

    feat: category entity

--- backend/src/modules/categories/entities/category.entity.ts
+++ backend/src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,16 @@
-export class Category {}
+import { Column, Entity, OneToMany } from 'typeorm';
+
+import { AbstractEntity } from '../../database/abstract.entity';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity({ name: 'categories' })
+export class Category extends AbstractEntity<Category> {
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description?: string;
+
+  @OneToMany(() => Product, (product) => product.category, { cascade: true })
+  products: Product[];
+}
