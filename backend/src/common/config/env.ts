17b0cf964d85ebcd8cecd2590b30f0cdd3c54b60
aZong <giongmua2004@gmail.com>
2025-09-17 11:22:54 +0700

    feat(env): get more env variables from .env

--- backend/src/common/config/env.ts
+++ backend/src/common/config/env.ts
@@ -5,7 +5,15 @@ const configService = new ConfigService();
 export const env = {
   jwtSecret: configService.getOrThrow('JWT_SECRET') as string,
   environment: configService.getOrThrow('ENVIRONMENT') as string,
+  sameSite: configService.getOrThrow<'lax' | 'strict' | 'none'>(
+    'SAME_SITE_MODE',
+  ),
   refreshTokenName: configService.getOrThrow('REFRESH_TOKEN_KEY') as string,
+  issue: configService.getOrThrow('ISSUER') as string,
+  audience: configService.getOrThrow('AUDIENCE') as string,
+  refreshExpiration: configService.getOrThrow('REFRESH_EXPIRATION'),
+  accessExpiration: configService.getOrThrow('ACCESS_EXPIRATION'),
+  cookieExpiration: configService.getOrThrow('COOKIE_EXPIRATION'),
 
   // Google
   googleClientId: configService.getOrThrow('GOOGLE_CLIENT_ID') as string,
