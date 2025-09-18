5cbaa0bbdc56028e166d7f970480bfbb141e00db
aZong <giongmua2004@gmail.com>
2025-09-19 00:20:29 +0700

    feat: update media storage

--- backend/src/common/utils/media-storage.ts
+++ backend/src/common/utils/media-storage.ts
@@ -2,13 +2,17 @@ import * as fs from 'fs';
 import { extname, join } from 'path';
 import { diskStorage } from 'multer';
 
-export const mediaStorage = (type: 'avatar' | 'cover' | 'product-images') => {
+export const mediaStorage = (
+  type: 'avatar' | 'cover' | 'product' | 'category',
+) => {
   return diskStorage({
     destination: (req, _, cb) => {
-      let folder: 'users' | 'products' = 'users';
+      let folder: 'users' | 'products' | 'categories' = 'users';
 
-      if (type === 'product-images') {
+      if (type === 'product') {
         folder = 'products';
+      } else if (type === 'category') {
+        folder = 'categories';
       }
 
       const id = req.params.id;
@@ -16,12 +20,22 @@ export const mediaStorage = (type: 'avatar' | 'cover' | 'product-images') => {
 
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
+      } else if (type === 'product') {
+        // TODO: Check the number of photos reached 6 pictures
+      } else if (type === 'category') {
+        // A category can have only one image.
+        // If the category folder already has an image, delete it before uploading a new one.
+
+        const files = fs.readdirSync(dir);
+        for (const file of files) {
+          fs.unlinkSync(join(dir, file));
+        }
       }
 
       cb(null, dir);
     },
     filename: (_, file, cb) => {
-      if (type === 'product-images') {
+      if (type === 'product' || type === 'category') {
         const uniqueSuffix = crypto.randomUUID();
         cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
       } else {
