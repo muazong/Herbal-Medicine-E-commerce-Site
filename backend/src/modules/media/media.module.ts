710ec69adc3db73d471d130bf40545ae555c735d
aZong <giongmua2004@gmail.com>
2025-09-19 00:22:15 +0700

    feat: add product and category modules

--- backend/src/modules/media/media.module.ts
+++ backend/src/modules/media/media.module.ts
@@ -1,16 +1,22 @@
-import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { forwardRef, Module } from '@nestjs/common';
 
 import { MediaService } from './media.service';
 import { Media } from './entities/media.entity';
+import { UsersModule } from '../users/users.module';
 import { User } from '../users/entities/user.entity';
 import { MediaController } from './media.controller';
-import { UsersModule } from '../users/users.module';
+import { ProductsModule } from '../products/products.module';
+import { Product } from '../products/entities/product.entity';
+import { Category } from '../categories/entities/category.entity';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Media, User]),
+    TypeOrmModule.forFeature([Media, User, Product, Category]),
     forwardRef(() => UsersModule),
+    ProductsModule,
+    CategoriesModule,
   ],
   controllers: [MediaController],
   providers: [MediaService],
