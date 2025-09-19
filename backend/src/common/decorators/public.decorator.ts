2a8163339f5c596fef1f3b2d2f0bfc7a8f323a76
aZong <giongmua2004@gmail.com>
2025-09-19 20:59:21 +0700

    chore: remove comment

--- backend/src/common/decorators/public.decorator.ts
+++ backend/src/common/decorators/public.decorator.ts
@@ -1,5 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 import { IS_PUBLIC_KEY } from '../contances';
 
-// NOTE: Remember to use it. OK :3
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
