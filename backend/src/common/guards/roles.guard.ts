8bcc024c89776a62290abad5d344850e54e5b106
aZong <giongmua2004@gmail.com>
2025-09-21 09:30:57 +0700

    feat: check isPublic

--- backend/src/common/guards/roles.guard.ts
+++ backend/src/common/guards/roles.guard.ts
@@ -2,12 +2,20 @@ import { Reflector } from '@nestjs/core';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 import { Role } from '../enums';
-import { ROLES_KEY } from '../contances';
+import { IS_PUBLIC_KEY, ROLES_KEY } from '../contances';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
