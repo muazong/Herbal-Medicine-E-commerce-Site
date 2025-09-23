b4ab984bf6e00b20b6582d1bdd4413aef2359978
aZong <giongmua2004@gmail.com>
2025-09-23 08:56:26 +0700

    feat: add cart module in cart item and export cart item service

--- backend/src/modules/cart-items/cart-items.module.ts
+++ backend/src/modules/cart-items/cart-items.module.ts
@@ -1,22 +1,25 @@
-import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { forwardRef, Module } from '@nestjs/common';
 import { CartItemsService } from './cart-items.service';
-import { CartItemsController } from './cart-items.controller';
 
 import { UsersModule } from '../users/users.module';
+import { CartsModule } from '../carts/carts.module';
 import { User } from '../users/entities/user.entity';
 import { Cart } from '../carts/entities/cart.entity';
 import { CartItem } from './entities/cart-item.entity';
 import { ProductsModule } from '../products/products.module';
 import { Product } from '../products/entities/product.entity';
+import { CartItemsController } from './cart-items.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([CartItem, Product, Cart, User]),
+    forwardRef(() => CartsModule),
     UsersModule,
     ProductsModule,
   ],
   controllers: [CartItemsController],
   providers: [CartItemsService],
+  exports: [CartItemsService],
 })
 export class CartItemsModule {}
