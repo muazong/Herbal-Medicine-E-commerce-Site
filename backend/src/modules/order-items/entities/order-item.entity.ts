2fe8e3a32e49ad3a80bb47b73149ad2c221f55f5
aZong <giongmua2004@gmail.com>
2025-09-24 10:01:41 +0700

    feat(entities): order and order item

--- backend/src/modules/order-items/entities/order-item.entity.ts
+++ backend/src/modules/order-items/entities/order-item.entity.ts
@@ -1,1 +1,33 @@
-export class OrderItem {}
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+import { OrderItemStatus } from '../../../common/enums';
+import { Order } from '../../orders/entities/order.entity';
+import { AbstractEntity } from '../../database/abstract.entity';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity({ name: 'order_items' })
+export class OrderItem extends AbstractEntity<OrderItem> {
+  @ManyToOne(() => Order, (order) => order.orderItems, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  order: Order;
+
+  @ManyToOne(() => Product, { onDelete: 'SET NULL', nullable: true })
+  @JoinColumn({ name: 'productId' })
+  product: Product;
+
+  @Column({ name: 'productId' })
+  productId: string;
+
+  @Column({ type: 'number', default: 0 })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  totalPrice: number;
+
+  @Column({
+    type: 'enum',
+    enum: OrderItemStatus,
+    default: OrderItemStatus.PENDING,
+  })
+  status: OrderItemStatus;
+}
--- backend/src/modules/orders/entities/order.entity.ts
+++ backend/src/modules/orders/entities/order.entity.ts
@@ -1,1 +1,35 @@
-export class Order {}
+import { Column, Entity, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+
+import { PaymentMethods } from '../../../common/enums';
+import { User } from '../../users/entities/user.entity';
+import { AbstractEntity } from '../../database/abstract.entity';
+import { OrderStatus } from '../../../common/enums/order-status.enum';
+import { OrderItem } from '../../order-items/entities/order-item.entity';
+
+@Entity({ name: 'orders' })
+export class Order extends AbstractEntity<Order> {
+  @ManyToOne(() => User, (user) => user.order, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column({ name: 'userId' })
+  userId: string;
+
+  @Column({ nullable: true })
+  shippingAddress?: string;
+
+  @Column({ type: 'enum', enum: PaymentMethods, default: PaymentMethods.CASH })
+  paymentMethod: PaymentMethods;
+
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING })
+  status: OrderStatus;
+
+  @OneToMany(() => OrderItem, (orderItem) => orderItem.order, {
+    eager: true,
+    cascade: true,
+  })
+  orderItems: OrderItem[];
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  totalPrice: number;
+}
