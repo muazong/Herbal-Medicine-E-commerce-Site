b21f573b1269e33a711feb6965131ba00282794e
aZong <giongmua2004@gmail.com>
2025-09-26 09:17:38 +0700

    feat: order items create dto

--- backend/src/modules/order-items/dto/create-order-item.dto.ts
+++ backend/src/modules/order-items/dto/create-order-item.dto.ts
@@ -1,1 +1,8 @@
-export class CreateOrderItemDto {}
+import { Product } from '../../products/entities/product.entity';
+import { Order } from '../../orders/entities/order.entity';
+
+export class CreateOrderItemDto {
+  order: Order;
+  product: Product;
+  quantity: number;
+}
