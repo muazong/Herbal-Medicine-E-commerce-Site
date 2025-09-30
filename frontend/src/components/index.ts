801f2b1af157ce849814287cd7a227cdc7018b65
aZong <giongmua2004@gmail.com>
2025-10-01 00:24:50 +0700

    feat: components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,9 @@
 import Header from './header/header';
 import Button from './button/button';
 import Profile from './profile/profile';
+import Products from './products/products';
+import Title from './title/title';
+import Categories from './categories/categories';
+import CardCategory from './card-category/card-category';
 
-export { Header, Button, Profile };
+export { Header, Button, Profile, Products, Title, Categories, CardCategory };
