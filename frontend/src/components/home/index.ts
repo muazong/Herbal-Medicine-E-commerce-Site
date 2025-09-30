5f766ab4f5a07b27ab151995a6a4ac62f1d2bae4
aZong <giongmua2004@gmail.com>
2025-10-01 01:05:27 +0700

    feat: proppose

--- frontend/src/components/home/index.ts
+++ frontend/src/components/home/index.ts
@@ -1,4 +1,5 @@
 import Hero from './hero/hero';
 import TrustBadges from './trust-badges/trust-badges';
+import Propose from './propose/propose';
 
-export { Hero, TrustBadges };
+export { Hero, TrustBadges, Propose };
