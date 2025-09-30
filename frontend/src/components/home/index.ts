3c71114dfce4be8dbce1281b59ed4fbc5455a785
aZong <giongmua2004@gmail.com>
2025-09-30 10:08:18 +0700

    feat(component): trust badges

--- frontend/src/components/home/index.ts
+++ frontend/src/components/home/index.ts
@@ -1,3 +1,4 @@
 import Hero from './hero/hero';
+import TrustBadges from './trust-badges/trust-badges';
 
-export { Hero };
+export { Hero, TrustBadges };
---
+++ frontend/src/components/home/trust-badges/trust-badges.tsx
@@ -0,0 +1,46 @@
+import styles from './trust-badges.module.css';
+
+import freeShipping from '@/assets/images/free_ship_no_bg.png';
+import twentyFourHours from '@/assets/images/24h_no_bg.png';
+import allowGoodsInspection from '@/assets/images/allow_goods_inspection_no_bg.png';
+import quality from '@/assets/images/quality_no_bg.png';
+import Image from 'next/image';
+
+function TrustBadges() {
+  return (
+    <section className={styles.container}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <div className={styles.itemImage}>
+            <Image src={freeShipping} alt="free shipping" fill />
+          </div>
+          <p>Miễn phí vận chuyển</p>
+        </li>
+        <li className={styles.item}>
+          <div className={styles.itemImage}>
+            <Image src={twentyFourHours} alt="twenty four hours" fill />
+          </div>
+          <p>Hỗ trợ 24h</p>
+        </li>
+        <li className={styles.item}>
+          <div className={styles.itemImage}>
+            <Image
+              src={allowGoodsInspection}
+              alt="allow goods inspection"
+              fill
+            />
+          </div>
+          <p>Kiểm tra hàng</p>
+        </li>
+        <li className={styles.item}>
+          <div className={styles.itemImage}>
+            <Image src={quality} alt="quality" fill />
+          </div>
+          <p>Đảm bảo chất lượng</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default TrustBadges;
