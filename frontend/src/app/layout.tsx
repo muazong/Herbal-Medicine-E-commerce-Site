c9340be6fcc5d4d35e9f1dffa1b87fbf85ca1397
aZong <giongmua2004@gmail.com>
2025-10-02 08:35:10 +0700

    feat: update root layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,3 @@
-import { Metadata } from 'next';
-
-import './globals.css';
-import { Header } from '@/components';
-import { roboto } from '@/common/fonts';
-
-export const metadata: Metadata = {
-  title: 'Anvita - An tâm từ thiên nhiên',
-  description:
-    'Chào mừng bạn đến với Anvita – nơi mang đến các sản phẩm Đông dược thiên nhiên, an toàn và hiệu quả. Cùng Anvita chăm sóc sức khỏe toàn diện cho bạn và gia đình."',
-  icons: {
-    icon: '/favicon.ico',
-  },
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,10 +5,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
-        <Header />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
