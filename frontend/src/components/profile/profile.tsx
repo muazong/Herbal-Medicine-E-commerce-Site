8d2c35496cc4ab595da4411becbf220d5f093b08
aZong <giongmua2004@gmail.com>
2025-10-03 01:16:26 +0700

    feat: check user is logged id

--- frontend/src/components/profile/profile.tsx
+++ frontend/src/components/profile/profile.tsx
@@ -5,15 +5,39 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 import styles from './profile.module.css';
-import logo from '@/assets/images/logo.png';
+import { CurrentUser } from '@/common/interfaces';
+import { api, env } from '@/common/config';
+import { removeAccessToken } from '@/common/lib/local-storage-actions';
 
-function Profile() {
+function Profile({ currentUser }: { currentUser: CurrentUser }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleLogout = async () => {
+    removeAccessToken();
+    try {
+      await api.post(
+        '/auth/logout',
+        {},
+        {
+          withCredentials: true,
+        },
+      );
+    } catch (error) {
+      console.log(error);
+    } finally {
+      window.location.reload();
+    }
+  };
 
   return (
     <div className={styles.container} onClick={() => setIsOpen(!isOpen)}>
       <div className={styles.avatar}>
-        <Image src={logo} alt="logo" fill className={styles.img} />
+        <Image
+          src={currentUser.avatar ? currentUser.avatar.path : env.AVATAR_URL}
+          alt="logo"
+          fill
+          className={styles.img}
+        />
       </div>
 
       <div
@@ -25,7 +49,9 @@ function Profile() {
             <Link href={''}>Tài khoản</Link>
           </li>
           <li>
-            <Link href={''}>Đăng xuất</Link>
+            <Link href={''} onClick={handleLogout}>
+              Đăng xuất
+            </Link>
           </li>
         </ul>
       </div>
