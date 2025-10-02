ef8e1a21a921de8a23abe2633b89b149ab54b6a2
aZong <giongmua2004@gmail.com>
2025-10-03 01:15:18 +0700

    feat: avatar and cover url

--- frontend/src/common/config/env.ts
+++ frontend/src/common/config/env.ts
@@ -1,4 +1,6 @@
 export const env = {
   API_URL: process.env.NEXT_PUBLIC_API_URL as string,
   ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN as string,
+  AVATAR_URL: process.env.NEXT_PUBLIC_AVATAR_URL as string,
+  COVER_URL: process.env.NEXT_PUBLIC_COVER_URL as string,
 };
