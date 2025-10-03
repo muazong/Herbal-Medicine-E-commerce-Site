1bed8cd168e0a00b872fcb91a52f6a06bc4f4b62
aZong <giongmua2004@gmail.com>
2025-10-03 22:09:08 +0700

    feat: update profile component

--- frontend/src/components/profile/profile.tsx
+++ frontend/src/components/profile/profile.tsx
@@ -2,30 +2,48 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from './profile.module.css';
 import { CurrentUser } from '@/common/interfaces';
-import { api, env } from '@/common/config';
+import { env } from '@/common/config';
+import { api } from '@/services';
+
 import { removeAccessToken } from '@/common/lib/local-storage-actions';
 
 function Profile({ currentUser }: { currentUser: CurrentUser }) {
   const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const handleLogout = async () => {
-    removeAccessToken();
     try {
-      await api.post(
+      const res = await api.post(
         '/auth/logout',
         {},
         {
           withCredentials: true,
         },
       );
+
+      if (res.status === 200) {
+        removeAccessToken();
+        window.location.reload();
+      }
     } catch (error) {
       console.log(error);
-    } finally {
-      window.location.reload();
     }
   };
 
@@ -43,13 +61,14 @@ function Profile({ currentUser }: { currentUser: CurrentUser }) {
       <div
         className={styles.dropdown}
         style={{ display: isOpen ? 'block' : 'none' }}
+        ref={ref}
       >
         <ul className={styles.dropdownList}>
           <li>
             <Link href={''}>Tài khoản</Link>
           </li>
           <li>
-            <Link href={''} onClick={handleLogout}>
+            <Link href="" onClick={handleLogout}>
               Đăng xuất
             </Link>
           </li>
