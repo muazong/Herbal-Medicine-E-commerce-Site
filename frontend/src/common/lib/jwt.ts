b90cc371ca6ed8d4e4a7d5825bdd7ab7fcf19b9d
aZong <giongmua2004@gmail.com>
2025-10-03 22:07:37 +0700

    feat(lib): jwt

---
+++ frontend/src/common/lib/jwt.ts
@@ -0,0 +1,20 @@
+import { jwtDecode } from 'jwt-decode';
+
+type JwtPayload = {
+  exp: number;
+  [key: string]: any;
+};
+
+function isJwtExpired(token: string): boolean {
+  try {
+    const { exp } = jwtDecode<JwtPayload>(token);
+    if (!exp) return true;
+
+    const now = Date.now() / 1000;
+    return exp < now;
+  } catch {
+    return true;
+  }
+}
+
+export { isJwtExpired };
