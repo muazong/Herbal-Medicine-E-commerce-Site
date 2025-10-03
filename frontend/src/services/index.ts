a2eafda07fe2abf5fabd773c0acd7546e6a8acfd
aZong <giongmua2004@gmail.com>
2025-10-03 22:07:50 +0700

    feat: services

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,3 +1,10 @@
 import { getCurrentUser } from './auth-service';
+import { getCurrentUserByRefreshToken } from './auth-service';
+import { api, apiWithRefreshToken } from './axios-instance';
 
-export { getCurrentUser };
+export {
+  api,
+  apiWithRefreshToken,
+  getCurrentUser,
+  getCurrentUserByRefreshToken,
+};
