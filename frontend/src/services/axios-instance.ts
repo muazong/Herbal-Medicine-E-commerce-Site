50207aaa868c328cfca2eac2ccb16b9c03e76661
aZong <giongmua2004@gmail.com>
2025-10-03 22:24:03 +0700

    feat: config NProgress with axios

--- frontend/src/services/axios-instance.ts
+++ frontend/src/services/axios-instance.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import { env } from '@/common/config';
+
+NProgress.configure({ showSpinner: false });
 
 const api = axios.create({
   baseURL: env.API_URL || '',
@@ -12,5 +16,21 @@ const apiWithRefreshToken = axios.create({
   baseURL: env.API_URL || '',
   withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+  NProgress.start();
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => {
+    NProgress.done();
+    return response;
+  },
+  (error) => {
+    NProgress.done();
+    return Promise.reject(error);
+  },
+);
 
 export { api, apiWithRefreshToken };
