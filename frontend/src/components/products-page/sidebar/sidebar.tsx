6abb811d5a1937c5c47c560ff60952111cdb7fd6
aZong <giongmua2004@gmail.com>
2025-10-04 16:45:12 +0700

    feat: fetch categories for sidebar

--- frontend/src/components/products-page/sidebar/sidebar.tsx
+++ frontend/src/components/products-page/sidebar/sidebar.tsx
@@ -1,27 +1,35 @@
+import { use } from 'react';
 import Link from 'next/link';
 import styles from './sidebar.module.css';
 
-import { categories } from '@/db';
 import { PATH } from '@/common/enums';
+import { getCategories } from '@/services/categories-service';
+import Title from '@/components/title/title';
 
 function Sidebar() {
+  const categories = use(getCategories());
+
   return (
-    <aside className={styles.container}>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link href={PATH.PRODUCTS}>Tất cả</Link>
-        </li>
-        {categories.map((category) => {
-          return (
-            <li key={category.id} className={styles.item}>
-              <Link href={`${PATH.PRODUCTS}/${category.id}`}>
-                {category.name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </aside>
+    <div className={styles.container}>
+      <Title text="Danh mục sản phẩm" className={styles.title} />
+      <aside className={styles.aside}>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link href={PATH.PRODUCTS}>Tất cả</Link>
+          </li>
+          {categories &&
+            categories.map((category) => {
+              return (
+                <li key={category.id} className={styles.item}>
+                  <Link href={`${PATH.PRODUCTS}?categoryId=${category.id}`}>
+                    {category.name}
+                  </Link>
+                </li>
+              );
+            })}
+        </ul>
+      </aside>
+    </div>
   );
 }
 
