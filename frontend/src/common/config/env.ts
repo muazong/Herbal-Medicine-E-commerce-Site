e3fc592c875d46a8e0d5b0c304bdf4060a0eafce
aZong <giongmua2004@gmail.com>
2025-10-04 09:07:43 +0700

    feat: add more env variables

--- frontend/src/common/config/env.ts
+++ frontend/src/common/config/env.ts
@@ -1,6 +1,8 @@
 export const env = {
+  SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL as string,
   API_URL: process.env.NEXT_PUBLIC_API_URL as string,
   ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN as string,
   AVATAR_URL: process.env.NEXT_PUBLIC_AVATAR_URL as string,
   COVER_URL: process.env.NEXT_PUBLIC_COVER_URL as string,
+  PRODUCT_URL: process.env.NEXT_PUBLIC_PRODUCT_URL as string,
 };
