e2b37f0b0240420e665a94a35bdc87aa7ef9fb5f
aZong <giongmua2004@gmail.com>
2025-10-04 16:44:18 +0700

    feat: get access token by refresh token

--- frontend/src/common/lib/local-storage-actions.ts
+++ frontend/src/common/lib/local-storage-actions.ts
@@ -1,6 +1,8 @@
 'use client';
 
+import { apiWithRefreshToken } from '@/services';
 import { env } from '../config';
+import { isJwtExpired } from './jwt';
 
 function setAccessToken(value: string) {
   localStorage.setItem(env.ACCESS_TOKEN, value);
@@ -14,4 +16,22 @@ function removeAccessToken() {
   localStorage.removeItem(env.ACCESS_TOKEN);
 }
 
-export { setAccessToken, getAccessToken, removeAccessToken };
+async function getAccessTokenFromFreshToken() {
+  const token = localStorage.getItem(env.ACCESS_TOKEN);
+
+  if (!token || isJwtExpired(token)) {
+    const res = await apiWithRefreshToken.get('/auth/refresh');
+    if (res.data.isInvalid) return null;
+
+    setAccessToken(res.data.accessToken);
+    return res.data.accessToken;
+  }
+  return token;
+}
+
+export {
+  setAccessToken,
+  getAccessToken,
+  removeAccessToken,
+  getAccessTokenFromFreshToken,
+};
