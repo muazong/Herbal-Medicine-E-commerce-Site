d150659acbbd41d879edd9021c33e05dac024093
aZong <giongmua2004@gmail.com>
2025-10-04 09:07:32 +0700

    fix: remove font weight 500

--- frontend/src/common/fonts/noto-serif.ts
+++ frontend/src/common/fonts/noto-serif.ts
@@ -2,6 +2,6 @@ import { Noto_Serif } from 'next/font/google';
 
 export const notoSerif = Noto_Serif({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '700'],
+  weight: ['300', '400', '700'],
   variable: '--font-noto-serif',
 });
