5b359d53c6b8a484a5fc26966afa06742a4bdf52
aZong <giongmua2004@gmail.com>
2025-10-06 08:29:26 +0700

    feat: add underline

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -4,17 +4,62 @@ import Link from 'next/link';
 import { PATH } from '@/common/enums';
 import styles from './navbar.module.css';
 import { usePathname } from 'next/navigation';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 
 function Navbar() {
   const pathname = usePathname();
+  const navRef = useRef<HTMLElement | null>(null);
+  const [underlineStyle, setUnderlineStyle] = useState({
+    left: 0,
+    width: 0,
+    opacity: 0,
+  });
+
+  useEffect(() => {
+    const activeLink = navRef.current?.querySelector(
+      `.${styles.active}`,
+    ) as HTMLElement | null;
+
+    if (activeLink) {
+      const { offsetLeft, offsetWidth } = activeLink;
+      setUnderlineStyle({
+        left: offsetLeft,
+        width: offsetWidth,
+        opacity: 1,
+      });
+    }
+  }, [pathname]);
+
+  const handleHover = (e: MouseEvent<HTMLElement>) => {
+    const { offsetLeft, offsetWidth } = e.target as HTMLElement;
+    setUnderlineStyle({ left: offsetLeft, width: offsetWidth, opacity: 1 });
+  };
+
+  const handleLeave = () => {
+    const activeLink = navRef.current?.querySelector(
+      `.${styles.active}`,
+    ) as HTMLElement | null;
+
+    if (activeLink) {
+      const { offsetLeft, offsetWidth } = activeLink;
+      setUnderlineStyle({
+        left: offsetLeft,
+        width: offsetWidth,
+        opacity: 1,
+      });
+    } else {
+      setUnderlineStyle((prev) => ({ ...prev, opacity: 0 }));
+    }
+  };
 
   return (
-    <nav className={styles.container}>
+    <nav className={styles.container} ref={navRef} onMouseLeave={handleLeave}>
       <ul className={styles.navList}>
         <li>
           <Link
             href={PATH.HOME}
             className={pathname.endsWith(PATH.HOME) ? styles.active : undefined}
+            onMouseEnter={handleHover}
           >
             TRANG CHỦ
           </Link>
@@ -25,6 +70,7 @@ function Navbar() {
             className={
               pathname.startsWith(PATH.PRODUCTS) ? styles.active : undefined
             }
+            onMouseEnter={handleHover}
           >
             SẢN PHẨM
           </Link>
@@ -35,6 +81,7 @@ function Navbar() {
             className={
               pathname.startsWith(PATH.CONTACT) ? styles.active : undefined
             }
+            onMouseEnter={handleHover}
           >
             LIÊN HỆ
           </Link>
@@ -45,11 +92,21 @@ function Navbar() {
             className={
               pathname.startsWith(PATH.ABOUT) ? styles.active : undefined
             }
+            onMouseEnter={handleHover}
           >
             GIỚI THIỆU
           </Link>
         </li>
       </ul>
+
+      <span
+        className={styles.underline}
+        style={{
+          left: underlineStyle.left,
+          width: underlineStyle.width,
+          opacity: underlineStyle.opacity,
+        }}
+      ></span>
     </nav>
   );
 }
