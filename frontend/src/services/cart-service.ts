08d63a6e04f0d78a313a0c29a1992fb50727d5ba
aZong <giongmua2004@gmail.com>
2025-10-10 11:17:17 +0700

    feat(service): cart services

--- frontend/src/services/cart-service.ts
+++ frontend/src/services/cart-service.ts
@@ -1,6 +1,7 @@
 import { AxiosError } from 'axios';
 import { apiWithAuth } from './axios-instance-client';
 import { PATH } from '@/common/enums';
+import { CartItem } from '@/common/interfaces';
 
 export async function addProductToCart(productId: string, quantity: number) {
   try {
@@ -16,5 +17,26 @@ export async function addProductToCart(productId: string, quantity: number) {
       return (window.location.href = PATH.LOGIN);
     }
     throw error;
+  }
+}
+
+export async function getProductsFromUserCart() {
+  try {
+    const response = await apiWithAuth.get('/carts/user/products');
+    if (!response) return null;
+    return response.data as CartItem[];
+  } catch {
+    return null;
+  }
+}
+
+export async function deleteProductFromUserCart(productId: string) {
+  try {
+    const response = await apiWithAuth.delete(`/carts/product/${productId}`);
+
+    if (!response) return null;
+    return response.data;
+  } catch {
+    return null;
   }
 }
