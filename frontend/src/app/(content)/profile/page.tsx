42fda0b633755816dcbdcc4bf9e9840df70aebb4
aZong <giongmua2004@gmail.com>
2025-10-11 23:34:56 +0700

    feat: handle update user data

--- frontend/src/app/(content)/profile/page.tsx
+++ frontend/src/app/(content)/profile/page.tsx
@@ -1,31 +1,31 @@
 'use client';
 
 import Image from 'next/image';
+import { toast } from 'sonner';
 import { FaPenToSquare } from 'react-icons/fa6';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 import { env } from '@/common/config';
 import styles from './page.module.css';
-import { User } from '@/common/interfaces';
-import { USER_ROLES } from '@/common/enums';
 import { getCurrentUser } from '@/services';
+import { useUserStore } from '@/stores/user-store';
+import { updateUser } from '@/services/user-service';
+import { ACCOUNT_STATUS, USER_ROLES } from '@/common/enums';
 import { apiWithAuth } from '@/services/axios-instance-client';
 
 export default function ProfilePage() {
-  const [user, setUser] = useState<User | null>(null);
+  const user = useUserStore((state) => state.getUser());
+  const setUser = useUserStore((state) => state.setUser);
   const [localAvatar, setLocalAvatar] = useState<string | null>(null);
   const [localCover, setLocalCover] = useState<string | null>(null);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const currentUser = await getCurrentUser();
-        setUser(currentUser);
-      } catch (err) {
-        console.error('Error loading user:', err);
-      }
-    })();
-  }, []);
+  if (!user) {
+    return (
+      <div style={{ height: '100vh', marginTop: 'var(--header-height)' }}>
+        <p>Không có thông tin của tài khoản.</p>
+      </div>
+    );
+  }
 
   const handleChangeImage = async (
     e: ChangeEvent<HTMLInputElement>,
@@ -45,18 +45,42 @@ export default function ProfilePage() {
       await apiWithAuth.post(`/users/${user.id}/${type}`, formData);
       const updatedUser = await getCurrentUser();
       setUser(updatedUser);
-    } catch (err) {
-      console.error(`Update ${type} failed:`, err);
+
+      toast.success(
+        type === 'avatar'
+          ? 'Cập nhật ảnh đại diện thành công!'
+          : 'Cập nhật ảnh bìa thành công!',
+      );
+    } catch {
+      toast.error('Cập nhật hình đại diện thất bại!');
     }
   };
+  const handleSaveChanges = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!user) return;
 
-  if (!user) {
-    return (
-      <div style={{ height: '100vh', marginTop: 'var(--header-height)' }}>
-        <p>Không có thông tin của tài khoản.</p>
-      </div>
-    );
-  }
+    const formData = new FormData(e.target as HTMLFormElement);
+    const firstName = formData.get('firstName') as string;
+    const lastName = formData.get('lastName') as string;
+    const phone = formData.get('phone') as string;
+    const address = formData.get('address') as string;
+    const status = formData.get('status') as ACCOUNT_STATUS;
+
+    const userUpdated = await updateUser(user.id, {
+      firstName,
+      lastName,
+      phone,
+      address,
+      status,
+    });
+
+    if (userUpdated) {
+      setUser(userUpdated);
+      toast.success('Cập nhật thành công!');
+    } else {
+      toast.error('Cập nhật thất bại!');
+    }
+  };
 
   const avatarSrc =
     localAvatar ||
@@ -129,17 +153,76 @@ export default function ProfilePage() {
         </div>
 
         <div className={styles.info}>
-          <h1>
-            {user.firstName} {user.lastName}
-          </h1>
-          <p className={styles.role}>
-            {user.role === USER_ROLES.ADMIN ? 'Quản trị viên' : 'Khách hàng'}
-          </p>
-          <p>Email: {user.email}</p>
-          {user.phone && <p>Số điện thoại: {user.phone}</p>}
-          {user.address && <p>Địa chỉ: {user.address}</p>}
-          <p>Đăng nhập bằng: {user.provider}</p>
-          <p>Trạng thái: {user.status}</p>
+          <h1>Thông tin người dùng</h1>
+          <form className={styles.form} onSubmit={handleSaveChanges}>
+            <label htmlFor="firstName">
+              Họ:{' '}
+              <input
+                id="firstName"
+                name="firstName"
+                defaultValue={user.firstName}
+                placeholder="Họ"
+              />
+            </label>
+
+            <label htmlFor="lastName">
+              Tên:{' '}
+              <input
+                id="lastName"
+                name="lastName"
+                defaultValue={user.lastName}
+                placeholder="Tên"
+              />
+            </label>
+
+            <label htmlFor="role">
+              Vai trò:{' '}
+              <p>
+                {user.role === USER_ROLES.ADMIN ? 'Quản trị' : 'Khách hàng'}
+              </p>
+            </label>
+
+            <label htmlFor="email">
+              Email: <p>{user.email}</p>
+            </label>
+
+            <label htmlFor="phone">
+              Số điện thoại:{' '}
+              <input
+                id="phone"
+                name="phone"
+                defaultValue={user.phone}
+                placeholder="Số diện thoại"
+              />
+            </label>
+
+            <label htmlFor="address">
+              Địa chỉ:{' '}
+              <input
+                id="address"
+                name="address"
+                defaultValue={user.address}
+                placeholder="Địa chỉ"
+              />
+            </label>
+
+            <label htmlFor="provider">
+              Đăng nhập bằng: <p>{user.provider}</p>
+            </label>
+
+            <label htmlFor="status">
+              Trạng thái:{' '}
+              <select defaultValue={user.status && user.status} name="status">
+                <option value={ACCOUNT_STATUS.ACTIVE}>Hoạt động</option>
+                <option value={ACCOUNT_STATUS.INACTIVE}>Ngừng hoạt động</option>
+                <option value={ACCOUNT_STATUS.BLOCKED}>Khoá tài khoản</option>
+              </select>
+            </label>
+
+            <button type="submit" className={styles.button}>
+              Lưu thay đổi
+            </button>
+          </form>
         </div>
       </div>
     </div>
