8e89c0e7cb4f31bcaa8a67e0a07d9c7df4578f91
aZong <giongmua2004@gmail.com>
2025-10-11 17:54:08 +0700

    feat(enum): account status

---
+++ frontend/src/common/enums/account-status.ts
@@ -0,0 +1,5 @@
+export enum ACCOUNT_STATUS {
+  BLOCKED = 'blocked',
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+}
--- frontend/src/common/enums/index.ts
+++ frontend/src/common/enums/index.ts
@@ -1,5 +1,6 @@
 import { PATH } from './path';
 import { USER_ROLES } from './user-roles';
 import { ACCOUNT_PROVIDERS } from './account-providers';
+import { ACCOUNT_STATUS } from './account-status';
 
-export { PATH, USER_ROLES, ACCOUNT_PROVIDERS };
+export { PATH, USER_ROLES, ACCOUNT_PROVIDERS, ACCOUNT_STATUS };
