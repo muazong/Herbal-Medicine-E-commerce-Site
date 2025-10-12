d923026146a2984e0aeede91b533a07098a16f43
aZong <giongmua2004@gmail.com>
2025-10-12 10:31:30 +0700

    feat(enum): payment method

--- frontend/src/common/enums/index.ts
+++ frontend/src/common/enums/index.ts
@@ -2,5 +2,6 @@ import { PATH } from './path';
 import { USER_ROLES } from './user-roles';
 import { ACCOUNT_PROVIDERS } from './account-providers';
 import { ACCOUNT_STATUS } from './account-status';
+import { PAYMENT_METHOD } from './paymen-method';
 
-export { PATH, USER_ROLES, ACCOUNT_PROVIDERS, ACCOUNT_STATUS };
+export { PATH, USER_ROLES, ACCOUNT_PROVIDERS, ACCOUNT_STATUS, PAYMENT_METHOD };
