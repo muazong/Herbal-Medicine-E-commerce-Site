e50ad639f240eb028e83fe99e8eeeac06d9329e9
aZong <giongmua2004@gmail.com>
2025-10-12 11:28:03 +0700

    feat(enum): order status

--- frontend/src/common/enums/index.ts
+++ frontend/src/common/enums/index.ts
@@ -3,5 +3,13 @@ import { USER_ROLES } from './user-roles';
 import { ACCOUNT_PROVIDERS } from './account-providers';
 import { ACCOUNT_STATUS } from './account-status';
 import { PAYMENT_METHOD } from './paymen-method';
+import { ORDER_STATUS } from './order-status';
 
-export { PATH, USER_ROLES, ACCOUNT_PROVIDERS, ACCOUNT_STATUS, PAYMENT_METHOD };
+export {
+  PATH,
+  USER_ROLES,
+  ACCOUNT_PROVIDERS,
+  ACCOUNT_STATUS,
+  PAYMENT_METHOD,
+  ORDER_STATUS,
+};
---
+++ frontend/src/common/enums/order-status.ts
@@ -0,0 +1,7 @@
+export enum ORDER_STATUS {
+  PENDING = 'pending',
+  CONFIRMED = 'confirmed',
+  SHIPPED = 'shipped',
+  DELIVERED = 'delivered',
+  CANCELLED = 'cancelled',
+}
