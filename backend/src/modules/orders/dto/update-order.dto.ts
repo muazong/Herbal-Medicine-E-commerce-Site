5faec1b17a0c70c3b708e8815ed75adeb0fdbc67
aZong <giongmua2004@gmail.com>
2025-10-14 23:03:08 +0700

    feat (orders): add phone number validation to update order dto

--- backend/src/modules/orders/dto/update-order.dto.ts
+++ backend/src/modules/orders/dto/update-order.dto.ts
@@ -1,7 +1,13 @@
-import { IsEnum, IsOptional, IsString, Length } from 'class-validator';
+import { IsEnum, IsOptional, IsString, Length, Matches } from 'class-validator';
 import { OrderStatus, PaymentMethods } from '../../../common/enums';
 
 export class UpdateOrderDto {
+  @Matches(/^(?:\+84|0)(?:3|5|7|8|9)\d{8}$/, {
+    message: 'Phone number is invalid',
+  })
+  @IsOptional()
+  phoneNumber: string;
+
   @IsString()
   @IsOptional()
   @Length(1, 255)
