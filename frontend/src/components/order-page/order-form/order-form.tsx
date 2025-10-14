ab34a439edf2c73cf3f6dcde585f72f895b3b955
aZong <giongmua2004@gmail.com>
2025-10-14 17:13:54 +0700

    feat(components): order form

--- frontend/src/components/order-page/order-form/order-form.tsx
+++ frontend/src/components/order-page/order-form/order-form.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import { toast } from 'sonner';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-import { PATH } from '@/common/enums';
+import { PATH, PAYMENT_METHOD } from '@/common/enums';
 import styles from './order-form.module.css';
 import { useUserStore } from '@/stores/user-store';
 import useCartItemsStore from '@/stores/cart-item-store';
-import { orderProducts } from '@/services/order-service';
+import { OrderFormData, orderProducts } from '@/services/order-service';
 import formatCurrency from '@/common/lib/format-currency';
+import { paymen_method_vi } from '@/common/config/paymen-method-vi';
 
 function OrderForm() {
   const router = useRouter();
@@ -34,7 +35,21 @@ function OrderForm() {
 
   const handleOrderSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const orders = await orderProducts();
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    const userName = formData.get('userName') as string;
+    const phoneNumber = formData.get('phoneNumber') as string;
+    const shippingAddress = formData.get('shippingAddress') as string;
+    const paymentMethod = formData.get('paymentMethod') as PAYMENT_METHOD;
+
+    const orderFormData: OrderFormData = {
+      paymentMethod,
+      phoneNumber,
+      shippingAddress,
+      userName,
+    };
+
+    const orders = await orderProducts(orderFormData);
 
     if (orders) {
       toast.success('Đặt hàng thành công!');
@@ -55,19 +70,38 @@ function OrderForm() {
         <input
           defaultValue={user.fullName}
           type="text"
-          name="fullName"
+          name="userName"
           required
         />
       </label>
+
       <label>
         Số điện thoại:
-        <input defaultValue={user.phone} type="tel" name="phone" required />
+        <input
+          defaultValue={user.phone}
+          type="tel"
+          name="phoneNumber"
+          required
+        />
       </label>
+
+      <label>
+        Phương thức thanh toán:
+        <select name="paymentMethod" required>
+          <option value={PAYMENT_METHOD.CASH}>
+            {paymen_method_vi[PAYMENT_METHOD.CASH]}
+          </option>
+          <option value={PAYMENT_METHOD.TRANSFER}>
+            {paymen_method_vi[PAYMENT_METHOD.TRANSFER]}
+          </option>
+        </select>
+      </label>
+
       <label>
         Địa chỉ giao hàng:
         <textarea
           defaultValue={user.address}
-          name="address"
+          name="shippingAddress"
           rows={3}
           required
         />
