cd5bdc083ade1fe493b00101d02fd7dcb5898d63
aZong <giongmua2004@gmail.com>
2025-10-14 23:45:22 +0700

    feat: fetch and display order with cancelation filter

--- frontend/src/components/ordered-page/ordered-list/ordered-list.tsx
+++ frontend/src/components/ordered-page/ordered-list/ordered-list.tsx
@@ -1,18 +1,40 @@
-import { Order } from '@/common/interfaces';
+'use client';
+
+import { useEffect } from 'react';
+
+import { ORDER_STATUS } from '@/common/enums';
 import styles from './ordered-list.module.css';
 import { order_status_vi } from '@/common/config';
 import formatDayVi from '@/common/lib/format-day-vi';
+import { useOrderStore } from '@/stores/order-store';
+import { getOrders } from '@/services/order-service';
 import OrderedItem from '../ordered-item/ordered-item';
 import formatCurrency from '@/common/lib/format-currency';
 
-function OrderedList({ orders }: { orders: Order[] }) {
-  if (!orders.length) {
+function OrderedList({ canceled = false }: { canceled?: boolean }) {
+  const orders = useOrderStore((state) => state.orders);
+  const setOrders = useOrderStore((state) => state.setOrders);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getOrders('createdAt');
+      setOrders(data || []);
+    })();
+  }, [setOrders]);
+
+  const filteredOrders = orders.filter((order) =>
+    canceled
+      ? order.status === ORDER_STATUS.CANCELLED
+      : order.status !== ORDER_STATUS.CANCELLED,
+  );
+
+  if (!filteredOrders.length) {
     return <div>Chưa có đơn hàng nào</div>;
   }
 
   return (
     <div className={styles.container}>
-      {orders.map((order) => {
+      {filteredOrders.map((order) => {
         return (
           <div key={order.id} className={styles.orderCard}>
             <div className={styles.orderHeader}>
