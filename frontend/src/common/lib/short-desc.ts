c58f788ede59f9d0cb07060cab08051a95b7a7e4
aZong <giongmua2004@gmail.com>
2025-12-08 14:20:18 +0700

    fix: check textLength

--- frontend/src/common/lib/short-desc.ts
+++ frontend/src/common/lib/short-desc.ts
@@ -1,6 +1,6 @@
 export function shortDesc(text: string, textLength: number = 12) {
   const words = text.trim().split(/\s+/);
-  if (words.length > 12) {
+  if (words.length > textLength) {
     return words.slice(0, textLength).join(' ') + '...';
   }
   return text;
