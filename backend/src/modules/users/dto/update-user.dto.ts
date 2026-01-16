305abfb46fca7bfbd07a92459454357d7be01321
aZong <giongmua2004@gmail.com>
2026-01-16 09:01:01 +0700

    fix: make field optional in update DTO

--- backend/src/modules/users/dto/update-user.dto.ts
+++ backend/src/modules/users/dto/update-user.dto.ts
@@ -19,6 +19,7 @@ export class UpdateUserDto {
   @Length(1, 50)
   lastName?: string;
 
+  @IsOptional()
   @Matches(/^(?:\+84|0)(?:3|5|7|8|9)\d{8}$/, {
     message: 'Phone number is invalid',
   })
